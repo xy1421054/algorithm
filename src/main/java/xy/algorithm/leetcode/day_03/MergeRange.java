ed1af4e18b7241e8c74e28429f324a8ac737eb70
xuyan <1592593614@qq.com>
2020-04-16 23:32:20 +0800

    leetCode-矩阵运算，暂时为理解 leetCode-区间范围，有思路，略懂后续补上

---
+++ src/main/java/xy/algorithm/leetcode/day_03/MergeRange.java
@@ -0,0 +1,60 @@
+package xy.algorithm.leetcode.day_03;
+
+import java.util.Arrays;
+
+/**
+ * 给出一个区间的集合，请合并所有重叠的区间。
+ *
+ * 示例 1:
+ *
+ * 输入: [[1,3],[2,6],[8,10],[15,18]]
+ * 输出: [[1,6],[8,10],[15,18]]
+ * 解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+ * 示例 2:
+ *
+ * 输入: [[1,4],[4,5]]
+ * 输出: [[1,5]]
+ * 解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-intervals
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MergeRange {
+
+    //TODO 略懂，思路正常。要不断回想
+
+    private static int[][] merge(int[][] intervals){
+//        for (int i = 0; i <intervals.length ; i++) {
+//            int[] interval = intervals[i];
+//            System.out.println(interval.toString());
+//        }
+        // 先按照区间起始位置排序
+        Arrays.sort(intervals, (v1, v2) -> v1[0] - v2[0]);
+        // 遍历区间
+        int[][] res = new int[intervals.length][2];
+        int idx = -1;
+        for (int[] interval: intervals) {
+            // 如果结果数组是空的，或者当前区间的起始位置 > 结果数组中最后区间的终止位置，
+            // 则不合并，直接将当前区间加入结果数组。
+            if (idx == -1 || interval[0] > res[idx][1]) {
+                res[++idx] = interval;
+            } else {
+                // 反之将当前区间合并至结果数组的最后区间
+                res[idx][1] = Math.max(res[idx][1], interval[1]);
+            }
+        }
+        int[][] ints = Arrays.copyOf(res, idx + 1); // TODO 需要复习下 Arrays.copyOf的用法
+
+        return ints;
+    }
+
+
+
+
+    public static void  main(String[] args){
+        int[][] inputParameter = new int[][]{{1,3},{2,6},{8,10},{15,18}};
+        int[][] merge = merge(inputParameter);
+
+    }
+}
